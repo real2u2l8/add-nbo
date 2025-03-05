808b633ebd8a2d9abb73e456e8d0788d72929111
SEOSLE <real2u2l8@gmail.com>
2025-03-05 16:23:43 +0900

    Update main.cpp - add exception handling

--- main.cpp
+++ main.cpp
@@ -3,14 +3,31 @@
 #include <netinet/in.h>
 
 int main(int argc, char* argv[]){
+    if (argc != 3) {
+        printf("사용법: %s <파일1> <파일2>\n", argv[0]);
+        return 1;
+    }
+
     FILE *aFile = fopen(argv[1], "rb");
     FILE *bFile = fopen(argv[2], "rb");
+
+    if (aFile == NULL || bFile == NULL) {
+        printf("파일을 열 수 없습니다.\n");
+        return 1;
+    }
 
     uint32_t aData;
     uint32_t bData;
     uint32_t sum;
-    fread(&aData, 4, 1, aFile);
-    fread(&bData, 4, 1, bFile);
+    
+    if (fread(&aData, 4, 1, aFile) != 1 || fread(&bData, 4, 1, bFile) != 1) {
+        printf("파일 읽기 실패\n");
+        return 1;
+    }
+
+    fclose(aFile);
+    fclose(bFile);
+
     aData = ntohl(aData);
     bData = ntohl(bData);
 
